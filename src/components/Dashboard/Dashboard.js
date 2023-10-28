f41a3c0a005224af4483a38a2cc745f2bd293491
jonathandow <89482514+jonathandow@users.noreply.github.com>
2023-10-28 17:26:28 -0400

    Added

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Dashboard.css';
+
+function Dashboard() {
+    return (
+        <div className="dashboard-container">
+            <h2>Welcome to the Dashboard</h2>
+        </div>
+    );
+}
+
+export default Dashboard;
