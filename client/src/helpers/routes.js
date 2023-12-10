860f4a528d56b3478cb7107da77c7384b0aea628
jonathandow <89482514+jonathandow@users.noreply.github.com>
2023-12-10 16:17:50 -0500

    Wrote API endpoints/routes

--- client/src/helpers/routes.js
+++ client/src/helpers/routes.js
@@ -1,2 +1,8 @@
 export const LOGIN_ROUTE = '/login';
 export const DASHBOARD_ROUTE = '/dashboard';
+export const PACKAGE_CREATE_ROUTE = '/package';
+export const PACKAGES_CREATE_ROUTE = '/packages';
+export const API_RESET_ROUTE = '/reset';
+export const USER_REGISTER_ROUTE = '/register';
+export const USER_AUTHENTICATE_ROUTE = '/authenticate';
+export const PACKAGE_RATE_ROUTE = '/package/:id/rate'
