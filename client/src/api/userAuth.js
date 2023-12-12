c841fd176ed5248973d667ae791c2e501b126df7
Rohan-Sagar <rsagar@purdue.edu>
2023-12-11 22:38:55 -0500

    Register User Fixed

--- client/src/api/userAuth.js
+++ client/src/api/userAuth.js
@@ -6,15 +6,14 @@ import {
 } from "../utils/axiosClient";
 import {
   LOGIN_API,
-  PACKAGE_API,
-  PACKAGES_API,
-  RESET_API,
+  CREATE_PACKAGE_API,
+  QUERY_PACKAGES_API,
+  RESET_REGISTRY_API,
   REGISTER_API,
-  AUTHENTICATE_API,
-  PACKAGE_RATE_API,
   GET_ALL_PACKAGES_API,
-  GET_PACKAGE_API,
 } from "../helpers/apiEndpoints";
+
+// Parameters: URL, params, navigate, requireToken, requireContentType
 
 export const getAllPackage = async (params, navigate) => {
   try {
@@ -42,40 +41,40 @@ export const loginUser = async (payload, navigate) => {
   }
 };
 
-export const getPackage = async (packageQueries, offset, navigate) => {
-  try {
-    const response = await postRequest(
-      GET_PACKAGE_API,
-      packageQueries,
-      navigate,
-    );
-    return response.type === 1 ? response.response : null;
-  } catch (error) {
-    console.error(`Error getting package details: `, error);
-  }
-};
+// export const getPackage = async (packageQueries, offset, navigate) => {
+//   try {
+//     const response = await postRequest(
+//       GET_PACKAGE_API,
+//       packageQueries,
+//       navigate,
+//     );
+//     return response.type === 1 ? response.response : null;
+//   } catch (error) {
+//     console.error(`Error getting package details: `, error);
+//   }
+// };
 
-export const createPackage = async (packageData, navigate) => {
-  try {
-    const response = await postRequest(PACKAGE_API, packageData, navigate);
-    return response.type === 1 ? response.response : null;
-  } catch (error) {
-    console.error("Error creating package: ", error);
-  }
-};
-
-export const createMultiplePackages = async (packagesData, navigate) => {
-  try {
-    const response = await postRequest(PACKAGES_API, packagesData, navigate);
-    return response.type === 1 ? response.response : null;
-  } catch (error) {
-    console.error("Error creating multiple packages: ", error);
-  }
-};
+// export const createPackage = async (packageData, navigate) => {
+//   try {
+//     const response = await postRequest(PACKAGE_API, packageData, navigate);
+//     return response.type === 1 ? response.response : null;
+//   } catch (error) {
+//     console.error("Error creating package: ", error);
+//   }
+// };
+//
+// export const createMultiplePackages = async (packagesData, navigate) => {
+//   try {
+//     const response = await postRequest(PACKAGES_API, packagesData, navigate);
+//     return response.type === 1 ? response.response : null;
+//   } catch (error) {
+//     console.error("Error creating multiple packages: ", error);
+//   }
+// };
 
 export const resetAPI = async (navigate) => {
   try {
-    const response = await postRequest(RESET_API, {}, navigate);
+    const response = await postRequest(RESET_REGISTRY_API, {}, navigate);
     return response.type === 1 ? response.response : null;
   } catch (error) {
     console.error("Error resetting API: ", error);
@@ -84,31 +83,28 @@ export const resetAPI = async (navigate) => {
 
 export const registerUser = async (userData, navigate) => {
   try {
-    const response = await postRequest(REGISTER_API, userData, navigate);
+    const response = await postRequest(
+      REGISTER_API,
+      userData,
+      navigate,
+      true,
+      false,
+    );
     return response.type === 1 ? response.response : null;
   } catch (error) {
     console.error("Error registering user: ", error);
   }
 };
 
-export const authenticateUser = async (userData, navigate) => {
-  try {
-    const response = await postRequest(AUTHENTICATE_API, userData, navigate);
-    return response.type === 1 ? response.response : null;
-  } catch (error) {
-    console.error("Error authenticating user: ", error);
-  }
-};
-
-export const getPackageRating = async (id, navigate) => {
-  try {
-    const response = await getRequest(
-      `${PACKAGE_RATE_API}/${id}`,
-      {},
-      navigate,
-    );
-    return response.type === 1 ? response.response : null;
-  } catch (error) {
-    console.error(`Error getting rating for package ${id}: `, error);
-  }
-};
+// export const getPackageRating = async (id, navigate) => {
+//   try {
+//     const response = await getRequest(
+//       `${PACKAGE_RATE_API}/${id}`,
+//       {},
+//       navigate,
+//     );
+//     return response.type === 1 ? response.response : null;
+//   } catch (error) {
+//     console.error(`Error getting rating for package ${id}: `, error);
+//   }
+// };
--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,183 +1,195 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Dashboard.css';
-import PackageDetails from '../Packages/PackageDetails'; 
-import { getAllPackage, getPackage, registerUser, createPackage, getPackageRating, createMultiplePackages, resetAPI } from '../../api/userAuth';
-
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Dashboard.css";
+import PackageDetails from "../Packages/PackageDetails";
+import {
+  getAllPackage,
+  // getPackage,
+  registerUser,
+  // createPackage,
+  getPackageRating,
+  createMultiplePackages,
+  resetAPI,
+} from "../../api/userAuth";
 
 function Dashboard() {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    const navigate = useNavigate();
-    const [isUserAuthenticated, setIsUserAuthenticated] = useState(true);
-    const [isAdmin, setIsAdmin] = useState(true);
-    const [showCreateUserForm, setShowCreateUserForm] = useState(false);
-    const [searchTerm, setSearchTerm] = useState("");
-    const [packages, setPackages] = useState([]); 
-    const [selectedPackage, setSelectedPackage] = useState(null);
-    const [sortKey, setSortKey] = useState('name');
-    const [newUsername, setNewUsername] = useState('');
-    const [newPassword, setNewPassword] = useState('');
-    const [isNewUserAdmin, setIsNewUserAdmin] = useState(false);
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  const navigate = useNavigate();
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(true);
+  const [showCreateUserForm, setShowCreateUserForm] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [packages, setPackages] = useState([]);
+  const [selectedPackage, setSelectedPackage] = useState(null);
+  const [sortKey, setSortKey] = useState("name");
+  const [newUsername, setNewUsername] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [isNewUserAdmin, setIsNewUserAdmin] = useState(false);
 
+  useEffect(() => {
+    loadPackages();
+  }, []);
 
-    
-    useEffect(() => {
-        loadPackages();
-    }, []);
+  const loadPackages = async () => {
+    try {
+      const params = { limit: 10, offset: 0 };
+      const response = await getAllPackage(params, navigate);
+      if (response && response.data) {
+        setPackages(response.data);
+      }
+    } catch (error) {
+      console.error("Error loading packages: ", error);
+    }
+  };
 
-    const loadPackages = async () => {
-        try {
-            const params = { limit: 10, offset: 0};
-            const response = await getAllPackage(params, navigate);
-            if (response && response.data) {
-                setPackages(response.data);
-            }
-        } catch (error) {
-            console.error('Error loading packages: ', error);
-        }
+  // const fetchPackageDetails = async (packageId) => {
+  //   try {
+  //     const response = await getPackage(packageId);
+  //     if (response && response.data) {
+  //       setSelectedPackage(response.data);
+  //     }
+  //   } catch (error) {
+  //     console.error(`Error fetching details for package ${packageId}: `, error);
+  //   }
+  // };
+
+  const handlePackageSelect = async (pkg) => {
+    setSelectedPackage(pkg);
+    const packageQueries = { Name: pkg.name, Version: pkg.version };
+    // await fetchPackageDetails(packageQueries);
+    // await handleRatePackage(pkg.id);
+  };
+
+  // const handleRatePackage = async (packageId) => {
+  //   try {
+  //     const rating = await getPackageRating(packageId);
+  //   } catch (error) {
+  //     console.error("Error getting rating: ", error);
+  //   }
+  // };
+  const handleCreateUser = async (event) => {
+    event.preventDefault();
+    try {
+      const userData = {
+        User: {
+          name: newUsername,
+          isAdmin: isNewUserAdmin,
+        },
+        Secret: {
+          password: newPassword,
+        },
+      };
+      await registerUser(userData);
+      setNewUsername("");
+      setNewPassword("");
+      setIsNewUserAdmin(false);
+      setShowCreateUserForm(false);
+    } catch (error) {
+      console.error("Error creating user: ", error);
+    }
+  };
+
+  const handleFileImport = async (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = async (event) => {
+      const packageData = JSON.parse(event.target.result);
+      // const newPackage = await createPackage(packageData);
+      // setPackages([...packages, newPackage]);
     };
+    reader.readAsText(file);
+  };
 
-    const fetchPackageDetails = async (packageId) => {
-        try {
-            const response = await getPackage(packageId);
-            if (response && response.data) {
-                setSelectedPackage(response.data);
-            }
-        }  catch (error) {
-            console.error(`Error fetching details for package ${packageId}: `, error);
-        }
-    };
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-header">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search for packages..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
 
-    const handlePackageSelect = async (pkg) => {
-        setSelectedPackage(pkg);
-        const packageQueries = { Name: pkg.name, Version: pkg.version };
-        await fetchPackageDetails(packageQueries);
-        await handleRatePackage(pkg.id);
-    };
+        <form className="import-form">
+          <label htmlFor="import-package">Import package:</label>
+          <input
+            type="file"
+            id="import-package"
+            accept=".zip"
+            onChange={handleFileImport}
+          />
+        </form>
 
-    const handleRatePackage = async (packageId) => {
-        try {
-            const rating = await getPackageRating(packageId);
-        } catch (error) {
-            console.error('Error getting rating: ', error);
-        }
-    };
-    const handleCreateUser = async  (event) => {
-        event.preventDefault();
-        
-        if (newUsername.length < 3){
-            alert('Username must be at least 3 characters long');
-            return;
-        }
-
-        if (!passwordRegex.test(newPassword)) {
-            alert('Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.');
-            return;
-        }
-
-
-            try {
-            const userData = {
-                username: newUsername, 
-                password: newPassword,
-                isAdmin: isNewUserAdmin
-            };
-            await registerUser(userData);
-            setNewUsername('');
-            setNewPassword('');
-            setIsNewUserAdmin(false);
-            setShowCreateUserForm(false);
-        } catch (error) {
-            console.error('Error creating user: ', error);
-        }
-    };
-
-    const handleFileImport = async (event) => {
-        const file = event.target.files[0];
-        const reader = new FileReader();
-        reader.onload = async (event) => {
-            const packageData = JSON.parse(event.target.result);
-            const newPackage = await createPackage(packageData);
-            setPackages([...packages, newPackage]);
-        };
-        reader.readAsText(file);
-    };
-
-    return (
-        <div className="dashboard-container">
-            <div className="dashboard-header">
-                <input 
-                    type="text"
-                    className="search-bar"
-                    placeholder="Search for packages..."
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-
-                <form className="import-form">
-                    <label htmlFor="import-package">Import package:</label>
-                    <input type="file" id="import-package" accept=".zip" onChange={handleFileImport} />
-                </form>
-
-                <div className="sort-filter">
-                    <label htmlFor="sort-packages">Sort by:</label>
-                    <select id="sort-packages" onChange={(e) => setSortKey(e.target.value)}>
-                        <option value="name">Name</option>
-                        <option value="dateAdded">Date Added</option>
-                    </select>
-                </div>
-            </div>
-
-            {isAdmin && (
-                    <button onClick={() => setShowCreateUserForm(true)}>
-                        Create New User
-                    </button>
-                )}
-
-            {showCreateUserForm && (
-                <form onSubmit={handleCreateUser}>
-                    <label>
-                        Username:
-                        <input 
-                            type="text" 
-                            value={newUsername}
-                            onChange={(e) => setNewUsername(e.target.value)}
-                            required 
-                        />
-                    </label>
-                    <label>
-                        Password:
-                        <input 
-                            type="password" 
-                            value={newPassword}
-                            onChange={(e) => setNewPassword(e.target.value)}
-                            required 
-                        />
-                    </label>
-                    <label>
-                        Is Admin:
-                        <input 
-                            type="checkbox" 
-                            checked={isNewUserAdmin}
-                            onChange={(e) => setIsNewUserAdmin(e.target.checked)}
-                        />
-                    </label>
-                    <button type="submit">Create User</button>
-                </form>
-            )}
-
-            <div className="package-list">
-                {packages.filter(pkg => pkg.name.includes(searchTerm)).map((pkg) => (
-                    <div key={pkg.id} className="package-item" onClick={() => handlePackageSelect(pkg)}>
-                        <span>{pkg.name}</span>
-                        <span>{pkg.dateAdded}</span>
-                    </div>
-                ))}
-            </div>
-
-            {selectedPackage && <PackageDetails package={selectedPackage} />}
+        <div className="sort-filter">
+          <label htmlFor="sort-packages">Sort by:</label>
+          <select
+            id="sort-packages"
+            onChange={(e) => setSortKey(e.target.value)}
+          >
+            <option value="name">Name</option>
+            <option value="dateAdded">Date Added</option>
+          </select>
         </div>
-    );
+      </div>
+
+      {isAdmin && (
+        <button onClick={() => setShowCreateUserForm(true)}>
+          Create New User
+        </button>
+      )}
+
+      {showCreateUserForm && (
+        <form onSubmit={handleCreateUser}>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={newUsername}
+              onChange={(e) => setNewUsername(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Is Admin:
+            <input
+              type="checkbox"
+              checked={isNewUserAdmin}
+              onChange={(e) => setIsNewUserAdmin(e.target.checked)}
+            />
+          </label>
+          <button type="submit">Create User</button>
+        </form>
+      )}
+
+      <div className="package-list">
+        {packages
+          .filter((pkg) => pkg.name.includes(searchTerm))
+          .map((pkg) => (
+            <div
+              key={pkg.id}
+              className="package-item"
+              onClick={() => handlePackageSelect(pkg)}
+            >
+              <span>{pkg.name}</span>
+              <span>{pkg.dateAdded}</span>
+            </div>
+          ))}
+      </div>
+
+      {selectedPackage && <PackageDetails package={selectedPackage} />}
+    </div>
+  );
 }
 
 export default Dashboard;
